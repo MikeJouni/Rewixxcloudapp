01392e3da9b0cf1ca6e6d5236cb61c7a53325bc8
khanabdu25 <khanabdu@umich.edu>
2025-07-12 00:17:05 -0400

    rm unused dep

--- backend/src/main/java/com/rewixxcloudapp/config/DotenvConfig.java
+++ backend/src/main/java/com/rewixxcloudapp/config/DotenvConfig.java
@@ -4,7 +4,6 @@ import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.core.env.Environment;
 
 @Configuration
 public class DotenvConfig {
