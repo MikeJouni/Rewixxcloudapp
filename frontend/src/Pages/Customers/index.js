bf7bdaf4b5853df3c04421dea52602e6fcd8f425
zsbeih <zsbeih@umich.edu>
2025-08-19 16:00:34 -0400

    removed incomplete customer details view

--- frontend/src/Pages/Customers/index.js
+++ frontend/src/Pages/Customers/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import CustomerListView from "./components/CustomerListView";
-import CustomerDetailView from "./components/CustomerDetailView";
 import CustomerCreateView from "./components/CustomerCreateView";
 
 const CustomersPage = () => {
@@ -9,7 +8,6 @@ const CustomersPage = () => {
     <Routes>
       <Route index element={<CustomerListView />} />
       <Route path="create" element={<CustomerCreateView />} />
-      <Route path=":id" element={<CustomerDetailView />} />
     </Routes>
   );
 };
