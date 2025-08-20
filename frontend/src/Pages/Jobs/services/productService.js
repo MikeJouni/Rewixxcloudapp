113fd2387c074e77d1efe180666c8b46a148e91c
zsbeih <zsbeih@umich.edu>
2025-08-19 22:03:56 -0400

    products post rather than get

--- frontend/src/Pages/Jobs/services/productService.js
+++ frontend/src/Pages/Jobs/services/productService.js
@@ -3,7 +3,7 @@ import Backend from "../../../Backend";
 export const getProducts = async () => {
   try {
     console.log("Fetching products from backend...");
-    const response = await Backend.get("api/products");
+    const response = await Backend.post("api/products/list");
     console.log("Products response:", response);
     return response;
   } catch (error) {
